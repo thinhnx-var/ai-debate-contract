28b9b508baae2dab29853a531561f836c0d5e7bd
ThinhNX <thinhnx@var-meta.com>
2025-01-26 14:49:00 +0700

    initial push

---
+++ contracts/MyToken.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.28;
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+// contract MyToken is ERC20, Ownable {
+//     constructor(address initialOwner, uint256 initialSupply) 
+//     ERC20("AI Debate", "AID") 
+//     Ownable(initialOwner)
+//     {
+//         _mint(msg.sender, initialSupply * (10 ** decimals()));
+//     }
+
+//     function mint(address to, uint256 amount) public onlyOwner {
+//         _mint(to, amount);
+//     }
+// }
